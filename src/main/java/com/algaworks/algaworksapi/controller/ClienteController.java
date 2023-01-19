3e2c652733210b795e34ffa06100e8547909c23d
Gabriel Barreto <gabriel.devloper@gmail.com>
2023-01-18 21:41:53 -0300

    feat: criando controlador e entidade de cliente

--- src/main/java/com/algaworks/algaworksapi/controller/ClienteController.java
+++ src/main/java/com/algaworks/algaworksapi/controller/ClienteController.java
@@ -1,14 +1,32 @@
 package com.algaworks.algaworksapi.controller;
 
+import com.algaworks.algaworksapi.entidades.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class ClienteController {
 
     @GetMapping("/clientes")
-    public String listar(){
-        return "teste";
+    public List<Cliente> listar() {
+
+        var cliente1 = new Cliente();
+        cliente1.setId(1L);
+        cliente1.setNome("James");
+        cliente1.setEmail("james@gmail.com");
+        cliente1.setTelefone("82981029294");
+
+        var cliente2 = new Cliente();
+        cliente2.setId(2L);
+        cliente2.setNome("maria");
+        cliente2.setEmail("maria@gmail.com");
+        cliente2.setTelefone("8198165165");
+
+        return Arrays.asList(cliente1, cliente2);
     }
 
 }
